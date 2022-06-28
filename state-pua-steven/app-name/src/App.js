9e784211d90772b21e2ded4b19804a3f347f9edb
pkamano <p.amano1225@protonmail.com>
2022-06-28 16:09:18 -0700

    Color Box challenge

--- state-pua-steven/app-name/src/App.js
+++ state-pua-steven/app-name/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ColorBox extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      colornames: "white"
+    }
+  }
+changeColor = () => {
+  this.setState({colornames: this.state.colornames + 1})
 }
 
-export default App;
+render(){
+  return (
+    <>
+    <h2>Color Box Mania</h2>
+    <p>colornames: {this.state.colornames}</p>
+    <button onClick={this.changecolor}>Change color</button>
+    </>
+  )
+}
+
+}
+
+export default ColorBox 
