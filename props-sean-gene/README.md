65c4079b4beeecdc438075cdbfba846cf8a6f29d
Genemichael25 <genemichael25@yahoo.com>
2022-06-30 16:42:37 -0700

    Updated props

--- props-sean-gene/README.md
+++ props-sean-gene/README.md
@@ -1,14 +1,11 @@
 As a user, I can see an application called Dice Roller
-
-
-
 As a developer, I can create a React application with App.js as my stateful component
+As a user, I can click a box and see the outcome of my current "roll"
 
 
 As a developer, I can create two child components that will accept props from App.js
 
 
-As a user, I can click a box and see the outcome of my current "roll"
 
 
 As a developer, I can pass a method from App.js to my dice component to display a number between 1 and 6
@@ -21,9 +18,8 @@ As a developer, I can pass the value of the roll to a log component
 
 
 As a user, I can see the roll log continue to grow as I roll the dice
+
 Stretch Goals
-
-
 As a user, I can see the image of a dice face when I "roll" the dice
 As a user, I can click a restart button that clears my roll log
 
--- props-sean-gene/src/App.js
+++ props-sean-gene/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import Dice from './components/Dice'
 import './App.css' 
+
 
 export class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       diceRoll: [1,2,3,4,5,6], 
-      currentRoll: 0
-      
+      currentRoll: 0,
+      oldRolls: []
     }
   }
   handleClick = () => {
@@ -18,11 +20,14 @@ export class App extends Component {
   render() {
     return (
       <>
-      <h1>Dice roller</h1> 
-      <p>They see me rolling, they hating, patrolling trying to catch me riding dirty... dice roller game</p>
+      <h1>Dice Roller</h1> 
+      <p>"They see me rolling, they hating, patrolling trying to catch me riding dirty..." - Chamillionaire</p>
       <br></br>
-      <p className = "itembox">{this.state.diceRoll[this.state.currentRoll]}</p>
-      <button onClick={this.handleClick}>click me</button>
+      <h2>Roll em</h2>
+      <Dice dice={this.state.diceRoll[this.state.currentRoll]}/>
+      <button onClick={this.handleClick}>Click to Roll</button>
+      <br></br>
+      <p></p>
       </>
 
 
