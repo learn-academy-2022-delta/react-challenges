3ef057be45c0bc8ce2fa7c9f04c29573e0d3bf72
Alvin Accad <alvinaccad5@gmail.com>
2022-07-05 13:02:24 -0700

    functional props

---
+++ func-props-alvin-luis/src/components/Price.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Price extends Component {
+  render() {
+    return (
+        <>
+        <ul>
+            {this.props.menuItems.map(item => {
+                return <li><button onClick={() => {this.props.plateOrder(item)}}>{item}</button></li>
+            })}
+        </ul>
+      </>
+    )
+  }
+}
+
+export default Price
